fb740292c22d296b17d8ee381f4aa43e712bda19
rebecalopez-gif <rebeca.curso2526@gmail.com>
2026-03-16 13:35:18 +0100

    constructor de creature creado extra, creadas ventanas habitacion ventana armario y la de nueva partida

--- src/vista/VentanaPartidaNew.java
+++ src/vista/VentanaPartidaNew.java
@@ -2,39 +2,92 @@ package vista;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class VentanaPartidaNew extends JDialog {
+import controlador.CriaturasControlador;
+import modelo.Creature;
 
-	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-	public VentanaPartidaNew() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+public class VentanaPartidaNew extends JDialog implements ActionListener{
+
+    private static final long serialVersionUID = 1L;
+    private final JPanel contentPanel = new JPanel();
+    private CriaturasControlador cont;
+    private JLabel lblNewLabel;
+    private JTextField nombreCriaturaField;
+    private JButton btnNewButton;
+    private JLabel lblNewLabel_1;
+    private Toolkit tk; //es para usar toda la pantalla supuestamente
+
+    public VentanaPartidaNew(CriaturasControlador controlador) {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\1dami\\Desktop\\PROYECTO FINAL\\FOTOS\\Monstruito adorable .png"));
+        this.cont = controlador;
+
+        // Pantalla completa para JDialog
+        tk = Toolkit.getDefaultToolkit();
+        int ancho = (int) tk.getScreenSize().getWidth();
+        int alto = (int) tk.getScreenSize().getHeight();
+
+        this.setSize(1349, 727);          
+        this.setLocationRelativeTo(null);
+        
+        getContentPane().setLayout(null);
+        contentPanel.setBounds(0, 0, 754, 494);
+        contentPanel.setLayout(null);
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        getContentPane().add(contentPanel);
+        
+        lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon("C:\\Users\\1dami\\Desktop\\PROYECTO FINAL\\FOTOS\\Fondo horizontal cri.png"));
+        lblNewLabel.setBounds(0, 0, 1539, 1048);
+        contentPanel.add(lblNewLabel);
+        
+        lblNewLabel_1 = new JLabel("Enter the creature’s name:");
+        lblNewLabel_1.setForeground(new Color(33, 143, 197));
+        lblNewLabel_1.setFont(new Font("Monospaced", Font.BOLD, 16));
+        lblNewLabel_1.setBounds(459, 378, 397, 51);
+        contentPanel.add(lblNewLabel_1);
+        
+        nombreCriaturaField = new JTextField();
+        nombreCriaturaField.setForeground(new Color(33, 143, 197));
+        nombreCriaturaField.setFont(new Font("Monospaced", Font.ITALIC, 14));
+        nombreCriaturaField.setBounds(459, 427, 353, 57);
+        contentPanel.add(nombreCriaturaField);
+        nombreCriaturaField.setColumns(10);
+        
+        btnNewButton = new JButton("PLAY");
+        btnNewButton.setBackground(new Color(70, 130, 180)); // azul bonito
+        btnNewButton.setForeground(Color.WHITE);
+        btnNewButton.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnNewButton.setFocusPainted(false);
+        btnNewButton.setBounds(616, 605, 186, 63);
+        getContentPane().add(btnNewButton);
+        btnNewButton.addActionListener(this);
+
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnNewButton) {
+			String nombre = nombreCriaturaField.getText();
+			Creature criatura = new Creature(nombre);
+
+			this.dispose();
+			VentanaHabitacion hab = new VentanaHabitacion(cont,criatura); 
+			hab.setVisible(true); 
 		}
+		
 	}
-
 }
