5b788af9cd2983121848e2c399e330beb9961882
rebecalopez-gif <rebeca.curso2526@gmail.com>
2026-03-16 12:17:06 +0100

    usergame hecho

---
+++ src/modelo/UserGame.java
@@ -0,0 +1,52 @@
+package modelo;
+
+import java.time.LocalDate;
+
+public class UserGame {
+	private String userName;
+	private String passwordUser;
+	private LocalDate birthDate;
+	
+	public UserGame() {
+		this.userName="";
+		this.passwordUser="";
+		this.birthDate=null;
+	}
+	
+	public UserGame(String userName,String passwordUser,LocalDate birthDate) {
+		this.userName=userName;
+		this.passwordUser=passwordUser;
+		this.birthDate=birthDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswordUser() {
+		return passwordUser;
+	}
+
+	public void setPasswordUser(String passwordUser) {
+		this.passwordUser = passwordUser;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return "UserGame [userName=" + userName + ", passwordUser=" + passwordUser + ", birthDate=" + birthDate + "]";
+	}
+	
+	
+}
