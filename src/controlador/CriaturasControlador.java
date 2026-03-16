e6a16e17c55226a5c162bbb0e3a45622f413d56d
ire220507-ctrl <ire220507@gamil.com>
2026-03-16 08:47:37 +0100

    Paquetes y Algnunas Ventanas

---
+++ src/controlador/CriaturasControlador.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import vista.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CriaturasControlador {
+	//UsuarioDAO dao = new ImplementacionBD();
+
+
+	public void visualizarPantalla() {
+	//	VentanaPrincipal ven = new VentanaPrincipal(this);
+		//ven.setVisible(true);	
+	}
+	
+	
+}
---
+++ src/principal/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package principal;
+import vista.*;
+import controlador.*;
+
+public class ProgramaPrincipal {
+	
+	public static void main(String[] args) {
+		CriaturasControlador cont = new CriaturasControlador();
+		cont.visualizarPantalla();
+	}
+}
